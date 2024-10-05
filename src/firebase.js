a99317a936922fdcaeb3b8c9f00ca7494eb50031
Akshit1718 <akshit4759.be23@chitkara.edu.in>
2024-10-05 10:00:25 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,63 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyBnmmwEJyRMc4tI8_JWGMXXjmVOBOvmRSQ",
+  authDomain: "deakin-app-8d49b.firebaseapp.com",
+  projectId: "deakin-app-8d49b",
+  storageBucket: "deakin-app-8d49b.appspot.com",
+  messagingSenderId: "811444407156",
+  appId: "1:811444407156:web:19c36e4c89046566663a21",
+  measurementId: "G-YZ2FRTMX15"
+};
+
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocFromAuth = async (userAuth, additionalInformation = {}) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation
+      })
+    }
+    catch (error) {
+      console.log('error in creating ', error.message)
+    }
+  }
+
+  return userDocRef;
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signinAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return await signInWithEmailAndPassword(auth, email, password)
+}
